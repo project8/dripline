e22547ac5acbdc0ea03cc8fa8fa86c473bd4bf65
Ben LaRoque <benjamin.laroque@gmail.com>
2014-06-16 14:23:27 -0700

    added ability to set P25V output

--- apps/dl_core/src/instruments/gen_gpib_spoller/agilent_e3631a.erl
+++ apps/dl_core/src/instruments/gen_gpib_spoller/agilent_e3631a.erl
@@ -37,14 +37,23 @@ start_link(InstrName, GPIBAddress, BusMod, BusName) ->
 
 handle_get(output, StateData) ->
     {send, <<"OUTP:STAT?">>, StateData};
+handle_get('25V', StateData) ->
+    {send, <<"APPL?">>, StateData};
 handle_get(_Any, StateData) ->
     lager:warning("unimplemented get"),
     {error, unimplemented, StateData}.
 
 handle_set(output, Value, StateData) ->
     {send, [<<"OUTP:STAT ">>, Value], StateData};
-handle_set(enable, Value, StateData) ->
-    {error, how_do_i_do_that, StateData};
+handle_set('25V', NewValue, StateData) ->
+    Branch = case unpack_value(NewValue) of
+        {ok, Parsed} ->
+            ToSend = ["APPL P25V,", Parsed],
+            {send, ToSend, StateData};
+        {error, Reason} ->
+            {error, Reason, StateData}
+    end,
+    Branch;
 handle_set(_Any, _Value, StateData) ->
     {error, unimplemented, StateData}.
 
@@ -75,3 +84,53 @@ handle_esr(1, StateData) ->
     {op_complete, StateData};
 handle_esr(Err, StateData) when Err =:= 8; Err =:= 33; Err =:= 9; Err =:= 32 ->
     {retrieve_error, <<"syst:err?">>, StateData}.
+
+
+%% Internal
+-spec unpack_value(binary()) -> {ok, binary()} | {error, term()}.
+unpack_value(Bin) ->
+    case binary:match(Bin, <<",">>) of
+        nomatch ->
+            parse_const_mode(Bin);
+        _Match ->
+            parse_reg_mode(Bin)
+    end.
+
+-spec parse_const_mode(binary()) -> {ok, term()} | {error, term()}.
+parse_const_mode(B) ->
+    case {is_v(B), is_c(B)} of
+        {true, false} ->
+            {ok, [drop_v(B), ", ", <<"MAX">>]};
+        {false, true} ->
+            {ok, [<<"MAX">>, ", ", drop_c(B)]};
+        _Other ->
+            {error, {badval, B}}
+    end.
+
+-spec parse_reg_mode(binary()) -> binary().
+parse_reg_mode(B) ->
+    [V, C] = binary:split(B, <<",">>),
+    case {is_v(V), is_c(V), is_v(C), is_c(C)} of
+        {true, false, false, true} ->
+            {ok, [drop_v(V), ", ", drop_c(C)]};
+        {false, true, true, false} ->
+            {ok, [drop_v(C), ", ", drop_c(V)]};
+        _Other ->
+            {error, {badval, B}}
+    end.
+
+-spec is_v(binary()) -> boolean().
+is_v(B) ->
+    binary:last(B) == $V.
+
+-spec is_c(binary()) -> boolean().
+is_c(B) ->
+    binary:last(B) == $A.
+
+-spec drop_v(binary()) -> binary().
+drop_v(Bin) ->
+    binary:replace(Bin, <<"V">>, <<>>).
+
+-spec drop_c(binary()) -> binary().
+drop_c(Bin) ->
+    binary:replace(Bin, <<"A">>, <<>>).
