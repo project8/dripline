791e5945ee08b9bee2da632115eabbf15c485cc2
Noah Oblath <nsoblath@gmail.com>
2016-10-07 11:47:53 -0700

    Basic server now repeates http request body

--- go/http2dripline/http2dripline.go
+++ go/http2dripline/http2dripline.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
     "fmt"
     "net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+    //fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "I received %s!", buf)
 }
 
 func main() {
