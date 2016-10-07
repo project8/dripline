dce7fa181a10b759e52a947ec968675e75e92803
Noah Oblath <nsoblath@gmail.com>
2016-10-07 12:19:58 -0700

    Separate requests and alerts by url

--- go/http2dripline/http2dripline.go
+++ go/http2dripline/http2dripline.go
@@ -13,7 +13,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "I received %s!", buf)
 }
 
+func RequestHandler(w http.ResponseWriter, r *http.Request) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	fmt.Fprintf(w, "Request received: %s", buf)
+}
+
+func AlertHandler(w http.ResponseWriter, r *http.Request) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	fmt.Fprintf(w, "Alert received: %s", buf)
+}
+
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/request/", RequestHandler)
+    http.HandleFunc("/alert/", AlertHandler)
     http.ListenAndServe(":8080", nil)
 }
